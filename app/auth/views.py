c6dbf6a45d890b1f4700461e5ef74d29694b67fc
panalexeu <pan.alexeu@gmail.com>
2023-10-20 23:36:34 +0300

    minor refactoring

--- app/auth/views.py
+++ app/auth/views.py
@@ -35,16 +35,17 @@ def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
+        # Adding user to db
         user = User(
             email=form.email.data,
             username=form.username.data,
             password=form.password.data
         )
-
         db.session.add(user)
         db.session.commit()
 
         flask.flash('Registration finished. You can now login.')
+
         return flask.redirect(flask.url_for('auth.login'))
 
     return flask.render_template('auth/register.html', form=form)
