3bd6151a47e53e5e28b86fb730e7b899dd44e0ed
panalexeu <pan.alexeu@gmail.com>
2023-10-20 20:45:22 +0300

    flash messages added, login implemented

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,6 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-from .main import main as main_blueprint
-from .auth import auth as auth_blueprint
 from config import configs
 
 
@@ -27,7 +25,10 @@ def create_app(config_name):
     login_manager.init_app(app)
 
     # Registering blueprints
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,6 @@
 import flask
-from flask_login import login_user
+import flask_login
+
 from . import auth
 from .forms import LoginForm
 from ..models import User
@@ -13,13 +14,24 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             # User exists, password is correct hence we can log him in
-            login_user(user, form.remember_me.data)
+            flask_login.login_user(user, form.remember_me.data)
 
             # Redirection handling
             next_ = flask.request.args.get('next')
             if next_ is None or not next_.startswith('/'):
-                # If next_ page doesn't exist or is malicious redirect to welcome page
+                # If next_ page doesn't exist or is malicious - redirect to welcome page
                 next_ = flask.url_for('main.welcome')
+
             return flask.redirect(next_)
+        else:
+            flask.flash('Invalid email or password.')
 
     return flask.render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@flask_login.login_required
+def logout():
+    flask_login.logout_user()
+    flask.flash('You have been logged out.')
+    return flask.redirect(flask.url_for('main.welcome'))
