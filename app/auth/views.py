9cdcd8005402c0f6b17121e0f051e54e37c52377
panalexeu <pan.alexeu@gmail.com>
2023-10-20 19:55:24 +0300

    login added

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,25 @@
-from flask import render_template
+import flask
+from flask_login import login_user
 from . import auth
 from .forms import LoginForm
+from ..models import User
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html', form=LoginForm())
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            # User exists, password is correct hence we can log him in
+            login_user(user, form.remember_me.data)
+
+            # Redirection handling
+            next_ = flask.request.args.get('next')
+            if next_ is None or not next_.startswith('/'):
+                # If next_ page doesn't exist or is malicious redirect to welcome page
+                next_ = flask.url_for('main.welcome')
+            return flask.redirect(next_)
+
+    return flask.render_template('auth/login.html', form=form)
