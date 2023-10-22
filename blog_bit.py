014672a647d8bac480f908d040cefd3f7ef649f2
panalexeu <pan.alexeu@gmail.com>
2023-10-22 15:24:27 +0300

    minor refactoring, email configuration added

--- blog_bit.py
+++ blog_bit.py
@@ -1,11 +1,11 @@
 import os
 
 from flask_migrate import Migrate
-from app import create_app, db, login_manager
+from app import create_app, db
 from app.models import User
 
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FLASK_CONFIG', 'default'))
 migrate = Migrate(app, db)
 
 
