2f9d95407e808b695d72acf4ffba609c627fed3b
panalexeu <pan.alexeu@gmail.com>
2023-10-23 15:47:22 +0300

    started implementing profiles system

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,15 @@
 from flask import render_template
+
 from . import main
+from ..models import User
 
 
 @main.route('/')
 def welcome():
     return render_template('main/welcome.html')
+
+
+@main.route('/profile/<username>')
+def profile(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('main/profile.html', user=user)
