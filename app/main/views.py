2fa14d5e6196d57cf50060a163ca594b42ec034f
panalexeu <pan.alexeu@gmail.com>
2023-10-25 18:55:03 +0300

    post editing implemented

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask_login import login_required, current_user
-from flask import render_template, flash, redirect, url_for, request, current_app
+from flask import render_template, abort, flash, redirect, url_for, request, current_app
 
 from . import main
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
@@ -108,3 +108,26 @@ def edit_profile_admin(id):
 def post(id):
     post = Post.query.get_or_404(id)
     return render_template('main/post.html', posts=[post])
+
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+
+    if current_user != post.author and not current_user.is_administrator():
+        abort(403)
+
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        db.session.commit()
+
+        flash('The post has been updated.')
+
+        return redirect(url_for('main.post', id=post.id))
+
+    form.body.data = post.body
+
+    return render_template('main/edit_post.html', form=form)
