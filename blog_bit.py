64412eeadfc9eff6057dcfb9f145508543d9d98c
panalexeu <pan.alexeu@gmail.com>
2023-11-21 16:26:51 +0200

    coverage implemented

--- blog_bit.py
+++ blog_bit.py
@@ -1,9 +1,10 @@
 import os
+import sys
+import click
 
 from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Role, Permission
-
 
 app = create_app(os.getenv('FLASK_CONFIG', 'default'))  # instantiating the app
 migrate = Migrate(app, db)  # to make migrations through terminal
@@ -20,7 +21,28 @@ def inject_permissions():
 
 
 @app.cli.command()
-def test():
+@click.option('--coverage/--no-coverage', default=False, help='Runs tests under code coverage.')
+def test(coverage):
+    COV = None
+
+    if coverage:
+        import coverage
+        COV = coverage.coverage(branch=True, include='app/*')
+        COV.start()
+
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if COV:
+        COV.stop()
+        COV.save()
+
+        print("Coverage Summary:")
+        COV.report()
+
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'reports\\coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file:\\\\%s\\index.html' % covdir)
+        COV.erase()
